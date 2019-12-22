34ef06ec3f37c5c9cb4d6aceb080b2b237084542
Paulo Tolentino - Cursos <paulotolentino.cursos@gmail.com>
2019-12-22 17:22:20 -0300

    Versão Inicial - Projeto Criado

---
+++ src/test/java/br/com/paulotolentino/cursomc/CursomcApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.paulotolentino.cursomc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursomcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
